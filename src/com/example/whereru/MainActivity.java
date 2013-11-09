718c75c498c11f2681ae0f3617b681f9e9e51438
chuhany <eeyangchuhan@gmail.com>
2013-11-09 11:23:28 -0500

    initial commit

---
+++ src/com/example/whereru/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.whereru;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends Activity {
+	static final LatLng MyLocation = new LatLng(40.44141, -79.94378);
+    private GoogleMap map;
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker mylocation = map.addMarker(new MarkerOptions().position(MyLocation).title("Hunt Libray").snippet("Here we are"));
+
+        // Move the camera instantly to NKUT with a zoom of 16.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(MyLocation, 16));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
