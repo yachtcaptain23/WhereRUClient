b0a07c2c23195eb154a4ab12959b4ca7aebd5500
chuhany <eeyangchuhan@gmail.com>
2013-11-09 22:34:15 -0500

    UI done

---
+++ src/com/example/whereru/ContactActivity.java
@@ -0,0 +1,25 @@
+package com.example.whereru;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.content.*;
+
+public class ContactActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact);
+		
+		Intent intent = getIntent();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.contact, menu);
+		return true;
+	}
+
+}
---
+++ src/com/example/whereru/SettingActivity.java
@@ -0,0 +1,25 @@
+package com.example.whereru;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.content.*;
+
+public class SettingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setting);
+		
+		Intent intent = getIntent();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.setting, menu);
+		return true;
+	}
+
+}
